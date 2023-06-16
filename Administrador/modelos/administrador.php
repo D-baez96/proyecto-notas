a96b6f55fe16ce2c4d92326ec2174aeaa4805c53
APRENDIZ <APRENDIZ@BOGDAFCGFFSP129.FormacionSena.RED>
2023-06-16 17:25:30 -0500

    Conexiones y Base de datos

--- Administrador/modelos/administrador.php
+++ Administrador/modelos/administrador.php
@@ -1,6 +1,57 @@
 <?php
-class Administrador 
+include_once('../../conexion.php');
+class Administrador extends connection
 {
+    public function __construct()
+    {
+        $this-> bd = parent::__construct();
+    }
+
+    //funcion para registrar los usuarios 
+
+    public function addAdmi($NombreUsu,$ApellidoUsu, $UsuarioUsu, $PasswordUsu, $Perfil, $EstadoUsu)
+    {
+    //crear sentncia slq
+    $statement = $this-> bd -> prepare("INSERT INTO usuarios(NombreUsu,ApellidoUsu,Usuario,Contraseña,Perfil,Estado) VALUES( :NombreUsu, :Usuario,  :Contraseña, :'Administrador', :'Activo')");
+
+    $statement -> bindParam(':NombreUsu',$NombreUsu)
+    $statement -> bindParam(':ApellidoUsu',$ApellidoUsu)
+    $statement -> bindParam(':Usuario',$Usuario)
+    $statement -> bindParam(':PasswordUsu',$PasswordUsu)
+    $statement -> bindParam(':Perfil',$Perfil)
+    $statement -> bindParam(':EstadoUsu',$EstadoUsu)
+
+    if($statement ->execute())
+    {
+        echo "Usuario registrado";
+        header (Location: '../pages/index.php');
+    }else{
+        echo "Usuario no registrado";
+        header (Location: '../agregar.php');
+    }
+    }
     
+//funcion para consultar todos los usuarios administradores
+    public function getAdmin()
+    {
+
+    }
+//funcion para consultar el usuario de acuerdo a su id
+    public function getIDadmin($id)
+    {
+
+    }
+
+    //funcion actualizar los datos de usuario
+    public function updateAd($id,$NombreUsu,$ApellidoUsu, $UsuarioUsu, $PasswordUsu, $EstadoUsu)
+    {
+
+    }
+
+    //funcion para eliminar usuario
+    public function deleteAd($id)
+    {
+
+    }
 }
 ?>
--- conexion.php
+++ conexion.php
@@ -1,8 +1,28 @@
 <?php
 class connection
 {
-    
+    protected $bd;
+    private $drive = "mysql";
+    private $host = "localhost";
+    private $namebd = "notas2023";
+    private $user = "root";
+    private $Password = "";
+
+    public function __construct()
+{
+    try {
+        $bd = new PDO("{$this-> drive}:host={$this-> host}; namebd={$this-> namebd}",$this->user, $this-> Password);
+
+        $bd->setAttribute (PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Conexión exitosa";
+        return $bd;
+        
+    }catch(PDOException $e){
+        echo "No se puede realizar la conexión".$e->getMessage();
+    }
 }
+}
+
 
 
 
