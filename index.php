4cbd035158a756b0dfa39d764c2549dc95b0febc
Daniel Baez <daniel-trabajo@hotmail.com>
2023-06-20 17:08:09 -0500

    index con estilos

--- index.php
+++ index.php
@@ -1,4 +1,24 @@
-<?php
-//pagina principal para ingresar al sistema
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles/styles.css">
+    <title>Inicia Sesion</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Inicia Sesion 🚀</h1>
+        <form action="">
+            <label for="nombre" class="labels" >Usuario</label>
+            <input type="text" placeholder="Escribe tu nombre aqui">
 
-?>
+            <label for="password" class="labels">Contraseña</label>
+            <input type="password" placeholder="Escribe tu contraseña aqui">
+
+        </form>
+        <button class="button"> ingresar</button>
+        <p><a href=""> Ingresa aqui</a> si no tienes cuenta</p>
+    </div>
+</body>
+</html>
