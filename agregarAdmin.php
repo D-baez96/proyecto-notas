4496b3a03febf97baf4cc10fe3dfd955a89a38d4
APRENDIZ <APRENDIZ@BOGDAFCGFFSP129.FormacionSena.RED>
2023-06-21 17:33:14 -0500

    Formularios

--- agregarAdmin.php
+++ agregarAdmin.php
@@ -3,26 +3,34 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" type="text/css" href="css/estilo.css">
+    <link rel="stylesheet" href="adminAgr.css">
     <title>Agregar</title>
-    
 </head>
 <body>
     <div class="contenedor">
         <form action="#">
-            <h2>Registrar</h2>
+            <h2>Registrar Administrador</h2>
+            <div class="inputbox">
             <label for="Nombre">Nombre: </label>
             <input type="text">
             <br>
+            </div>
+            <div class="inputbox">
             <label for="Apellido">Apellido: </label>
             <input type="text">
             <br>
+            </div>
+            <div class="inputbox">
             <label for="NombreUsu">Nombre de usuario: </label>
             <input type="text">
             <br>
+            </div>
+            <div class="inputbox">
             <label for="Contraseña">Contraseña </label>
             <input type="password">
             <br>
+            <input type="submit" class="boton">
+            </div>
         </form>
     </div>
 </body>
---
+++ agregarEstu.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Document</title>
+</head>
+<body>
+<div class="contenedor">
+        <form action="#">
+            <h2>Registrar Estudiante</h2>
+            <div class="inputbox">
+            <label for="Nombre">Nombre: </label>
+            <input type="text">
+            <br>
+            </div>
+            <div class="inputbox">
+            <label for="Apellido">Apellido: </label>
+            <input type="text">
+            <br>
+            </div>
+            <div class="inputbox">
+            <label for="Documento">Documento </label>
+            <input type="text">
+            <br>
+            </div>
+            <div class="inputbox">
+            <label for="Materia">Materia</label>
+            <input type="text">
+            <br>
+            </div>
+            <div class="inputbox">
+            <label for="Docente">Docente</label>
+            <input type="text">
+            <br>
+            </div>
+            <div class="inputbox">
+            <label for="fecha">Fecha de registro </label>
+            <input type="date">
+            <br>
+            </div>
+            <div class="inputbox">
+            <label for="Correo">Correo</label>
+            <input type="email">
+            <br>
+            </div>
+            <div class="inputbox">
+            <label for="Contraseña">Contraseña </label>
+            <input type="password">
+            <br>
+            <input type="submit" class="boton">
+            </div>
+        </form>
+    </div>
+</body>
+</html>
